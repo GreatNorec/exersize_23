8b1a0a5f41e7acf31b061d0a520e53352e241064
GN <andreifootball@yandex.ru>
2023-03-09 23:52:29 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число которое будем возводить в степень: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    Console.Write(Math.Pow(i, 3));
+    if (i < N) Console.Write(", ");
+}
